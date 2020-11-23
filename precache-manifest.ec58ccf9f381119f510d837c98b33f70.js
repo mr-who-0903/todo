68a419e3d4212af484a0d3240bf22e249b9f6154
unknown <ritikjain0903@gmail.com>
2020-11-23 17:50:27 +0530

    Updates

---
+++ precache-manifest.ec58ccf9f381119f510d837c98b33f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9021cf3db4ea4a06dd7129a158eeeb67",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "15617ad254e3a7bec0ad",
+    "url": "/todo/static/css/main.f48b8e5e.chunk.css"
+  },
+  {
+    "revision": "3fa3e2fa8092525544f2",
+    "url": "/todo/static/js/2.449fbf76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.449fbf76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15617ad254e3a7bec0ad",
+    "url": "/todo/static/js/main.fe21077f.chunk.js"
+  },
+  {
+    "revision": "86e1c9d45682970d456b",
+    "url": "/todo/static/js/runtime-main.f5de08d2.js"
+  }
+]);
